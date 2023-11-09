764f22ec71f4a27459c79c1ff56cfff83c62254f
RajKit || 鬼上電腦 <108155637+kdRajKit@users.noreply.github.com>
2023-11-09 17:14:18 +0100

    Update stdafx.h

--- RajKit_USER/RajKit_USER/stdafx.h
+++ RajKit_USER/RajKit_USER/stdafx.h
@@ -1,7 +1,3 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
 
 #pragma once
 
@@ -9,7 +5,3 @@
 
 #include <stdio.h>
 #include <tchar.h>
-
-
-
-// TODO: reference additional headers your program requires here
